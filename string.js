5d3cf9d987efa65e11388570222d8f91f4ed232f
Jerry1001-g <jerryalabi62@gmail.com>
2025-03-03 01:23:28 +0100

    feat: add file

---
+++ string.js
@@ -0,0 +1,3 @@
+let string=  "    Hello, World! This is a TEST string. It has 123 numbers and some extra spaces.   ";
+console.log(string.length); // 74
+console.log(string.toLowerCase()); // hello, world! this is a test string. it has 123 numbers and some extra spaces.
